ad8c89d07ccbf34d6adff67d448ac05cf3457d1f
admin <admin@DESKTOP-7EPVFAQ>
2020-09-17 00:25:28 +0000

    0917 09:25 선호 main account/스티커 검색 기능 주석

--- mypage/src/main/java/com/ch/mypage/controller/OpController.java
+++ mypage/src/main/java/com/ch/mypage/controller/OpController.java
@@ -1,15 +1,9 @@
 package com.ch.mypage.controller;
 
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.ch.mypage.model.ObjectPosition;
 import com.ch.mypage.model.Textbox;
 import com.ch.mypage.service.OpService;
 
